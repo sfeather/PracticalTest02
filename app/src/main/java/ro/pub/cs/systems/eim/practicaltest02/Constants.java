e3ede12ad0c0920a03f88f0031d235bbe87cc7d0
Stefan Pana <stefanpana00@gmail.com>
2023-05-25 08:12:25 +0300

    sub 1

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/Constants.java
@@ -0,0 +1,5 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+public class Constants {
+    final public static String TAG = "[PracticalTest02]";
+}
---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -0,0 +1,29 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(Constants.TAG, "[MAIN ACTIVITY] onCreate() callback method has been invoked");
+
+        setContentView(R.layout.activity_practical_test02_main);
+    }
+}
